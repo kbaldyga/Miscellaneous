9a484d5fba702cf24198da8888a7de0a50bc9241
Kamil Bałdyga <kbaldyga@gmail.com>
2014-04-13 11:42:56 +0100

    codejam 2014 qualification round

---
+++ codejam_2014/A-Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace A
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int repetitions = int.Parse(Console.ReadLine ());
+			for(int i = 1 ; i <= repetitions ; i ++) {
+				uint bitSet1 = 0;
+				uint bitSet2 = 0;
+
+				for (int question = 0; question < 2; question++) { 
+					var selectedLine = int.Parse (Console.ReadLine ());
+					for (int row = 0; row < 4; row++) {
+						if (row != selectedLine - 1) {
+							Console.ReadLine ();
+						} else {
+							var numbers = Console.ReadLine ().Split (new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+							.Select (n => int.Parse (n));
+							foreach (var number in numbers) {
+								if (question == 1) {
+									bitSet1= bitSet1 | (uint)(1 << number);
+								} else {
+									bitSet2 = bitSet2 | (uint)(1 << number);
+								}
+							}
+						}
+					}
+				}
+
+				var numberOfBits = NumberOfSetBits (bitSet1 & bitSet2);
+
+				switch(numberOfBits) {
+					case 0:
+					Console.WriteLine ("Case #{0}: Volunteer cheated!", i);
+						break;
+					case 1:
+						uint v = bitSet1 & bitSet2;
+						int number = MultiplyDeBruijnBitPosition[((uint)((v & -v) * 0x077CB531U)) >> 27];
+						Console.WriteLine ("Case #{0}: {1}", i, number);
+						break;
+					default:
+						Console.WriteLine ("Case #{0}: Bad magician!", i);
+						break;
+				}
+			}
+
+		}
+
+		static readonly int[] MultiplyDeBruijnBitPosition =  {
+				0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 
+				31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+			};
+
+		static uint NumberOfSetBits(uint i)
+		{
+			i = i - ((i >> 1) & 0x55555555);
+			i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+			return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+		}
+	}
+}
---
+++ codejam_2014/B-Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace B
+{
+	class MainClass
+	{
+		static readonly decimal[] cache = new decimal[100000];//null; // set size to 50000 and set 0th el to 0
+
+		public static void Main (string[] args)
+		{
+			System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+			customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+			System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+			int cases = int.Parse (Console.ReadLine ());
+			for(int t = 1 ; t <= cases ; t ++) {
+				var numbers = Console.ReadLine ()
+					.Split (new[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries)
+					.Select (n => decimal.Parse (n))
+					.ToArray();
+				var c = numbers [0];
+				var f = numbers [1];
+				var x = numbers [2];
+
+
+				var currentBest = result(0, x, f, c);
+
+				for(uint j = 1 ; j < 100000; j ++) {
+					var currentResult = result (j, x, f, c);
+					if(currentResult < currentBest ) {
+						currentBest = currentResult;
+					} else if (currentResult > currentBest) {
+						break;
+					}
+				}
+
+				Console.WriteLine ("Case #{0}: "+ currentBest.ToString("0.0000000").Replace(" ",""), t);
+			}
+		}
+
+		static decimal result(uint n, decimal x, decimal f, decimal c) {
+			return x / (2.0m + (decimal)n * f) + ff (n, c, f);
+		}
+
+		static decimal ff(uint n, decimal c, decimal f) {
+			if (n == 0)
+				return (cache[0] = 0.0m);
+
+			var current = c / (2.0m + (decimal)(n - 1) * f);
+			cache [n] = cache [n - 1] + current;
+			return cache [n];
+		}
+	}
+}
