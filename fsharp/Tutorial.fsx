98a51e2ce08163bb1fce548d9471de42097cfb2c
kbaldyga <kbaldyga@gmail.com>
2015-06-24 09:31:19 +0200

    Update Tutorial.fsx
    updated with some extra info

--- fsharp/Tutorial.fsx
+++ fsharp/Tutorial.fsx
@@ -1,4 +1,4 @@
-﻿#r "..\\packages\\FSharp.Charting.0.90.10\\lib\\net40\\FSharp.Charting.dll"
+#r "..\\packages\\FSharp.Charting.0.90.10\\lib\\net40\\FSharp.Charting.dll"
 
 open System
 open System.Globalization
@@ -7,6 +7,15 @@ open FSharp.Charting
 type Description =
       Transfer of string 
     | DirectDebit of string
+    | Groceries of string
+    | OrganicGroceries of string
+    | HemaAndStuff of string
+    | Salary of string
+    | Rent of string
+    | Clothes of string
+    | PublicTransport of string
+    | Outing of string
+    | Cash of string
 
 type Transaction = { 
     id : int;
@@ -22,6 +31,11 @@ type Transaction = {
 let stringToDate d = System.DateTime.ParseExact(d, "yyyymmdd", null)
 let stringToDecimal (s:string) = s.Replace(",", ".") |> decimal
 
+// used to groupBy Description types
+let getTag (a:Transaction) = 
+  let (uc,_) = Microsoft.FSharp.Reflection.FSharpValue.GetUnionFields(a.description, typeof<Description>)
+  uc.Name
+
 let (|RegexContains|_|) pattern input = 
     let matches = System.Text.RegularExpressions.Regex.Matches(input, pattern)
     if matches.Count > 0 then Some input
@@ -29,10 +43,45 @@ let (|RegexContains|_|) pattern input =
 
 let getDescription desc = 
     match desc with
-     | RegexContains "BEA*" desc -> Transfer (desc.Substring(33))
+     | RegexContains "BEA.*EkoPlaza" desc -> OrganicGroceries(desc.Substring(33, 20))
+     | RegexContains "BEA.*Biomarkt" desc -> OrganicGroceries(desc.Substring(33, 20))
+     | RegexContains "BEA.*Marqt" desc -> OrganicGroceries(desc.Substring(33, 20))
+     | RegexContains "Estafette Amsterdam" desc -> OrganicGroceries(desc.Substring(33, 20))
+
+     | RegexContains "BEA.*Stach BV" desc -> Groceries(desc.Substring(33, 20))
+     | RegexContains "BEA.*ALBERT HEIJN" desc -> Groceries(desc.Substring(33, 20))
+     | RegexContains "BEA.*Slagerij" desc -> Groceries(desc.Substring(33, 20))
+     | RegexContains "SUPERMARKT 'DE MAROK" desc -> Groceries(desc.Substring(33, 20))
+     | RegexContains "Toko Dun Yong" desc -> Groceries(desc.Substring(33, 20))
+
+     | RegexContains "BEA.*HEMA" desc -> HemaAndStuff(desc.Substring(33, 20))
+     | RegexContains "BEA.*Kruidvat" desc -> HemaAndStuff(desc.Substring(33, 20))
+     | RegexContains "BEA.*Chris Bloemsierkunst" desc -> HemaAndStuff(desc.Substring(33, 20))
+     | RegexContains "APOTHE" desc -> HemaAndStuff(desc.Substring(33, 20))
+
+     | RegexContains "Travix Nederland BV" decs -> Salary desc
+     | RegexContains "/Booking.com Customer Service Center" desc -> Salary desc
+
+     | RegexContains "RABONL2U/NAME/Tom/REMI" desc -> Rent desc
+
+     | RegexContains "Outlet A'dam AMSTERDAM Z" desc -> Clothes desc
+     | RegexContains "H&M" desc -> Clothes desc
+
+     | RegexContains "GVB" desc | RegexContains "NS-A'dam" desc -> PublicTransport desc
+
+     | RegexContains "ING AMSTERDAM" desc | RegexContains "ATM 245 AGENCE CENTRE LU" desc -> Cash desc
+     
+     | RegexContains "POULE-MOULES BRUGGE" desc -> Outing desc
+     | RegexContains "CafeRestLand&Zeezich" desc -> Outing desc
+     | RegexContains "BURGERMEESTER" desc -> Outing desc
+     | RegexContains "Pacific Parc BV" desc -> Outing desc
+     | RegexContains "Buurman en Buurman" desc -> Outing desc
+     | RegexContains "MCDONALD" desc -> Outing desc
+     
+     | RegexContains "BEA" desc -> Transfer (desc.Substring(33, 20))
      | _ -> DirectDebit desc
 
-let data = System.IO.File.ReadAllLines "c:\input.TAB" 
+let data = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.TAB") 
             |> Seq.map(fun line -> line.Split [|'\t'|]) 
             |> Seq.map (fun tokens -> 
                 { id = int(tokens.[0]); 
@@ -45,4 +94,12 @@ let data = System.IO.File.ReadAllLines "c:\input.TAB"
                     description = getDescription tokens.[7];
                 })
 
-Chart.Point [for x in data -> x.transaction]
+
+let groups = data |> 
+                Seq.groupBy getTag |>
+                Seq.map (fun (group, list) -> 
+                    (group, Seq.sumBy (fun i -> i.transaction) list))
+
+let chart = Chart.Pie groups;;
+
+chart.ShowChart();;
